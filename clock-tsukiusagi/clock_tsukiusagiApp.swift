b3fa97fef1d2bae7f267a8d043e03c59e4e72348
azu-azu <azu-azu@users.noreply.github.com>
2025-10-18 09:17:41 +0900

    Initial Commit

---
+++ clock-tsukiusagi/clock_tsukiusagiApp.swift
@@ -0,0 +1,17 @@
+//
+//  clock_tsukiusagiApp.swift
+//  clock-tsukiusagi
+//
+//  Created by 松本和実 on 2025/10/18.
+//
+
+import SwiftUI
+
+@main
+struct clock_tsukiusagiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ clock-tsukiusagiTests/clock_tsukiusagiTests.swift
@@ -0,0 +1,17 @@
+//
+//  clock_tsukiusagiTests.swift
+//  clock-tsukiusagiTests
+//
+//  Created by 松本和実 on 2025/10/18.
+//
+
+import Testing
+@testable import clock_tsukiusagi
+
+struct clock_tsukiusagiTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
